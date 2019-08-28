9c4ad771b59201d5bbd431c6ec4b1c0edd898c3c
hufei.shen <hufei.shen@ttpai.cn>
2019-08-28 11:26:59 +0800

    集合类源码学习

--- src/main/java/com/shenhufei/Katyusha/utils/TestCollectionsUtils.java
+++ src/main/java/com/shenhufei/Katyusha/utils/TestCollectionsUtils.java
@@ -1,9 +1,20 @@
 package com.shenhufei.Katyusha.utils;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class TestCollectionsUtils {
 
 	public static void main(String[] args) {
-
+		 List<String> names1 = new ArrayList<String>();
+	      names1.add("Google ");
+	      names1.add("Runoob ");
+	      names1.add("Taobao ");
+	      names1.add("Baidu ");
+	      names1.add("Sina ");
+	      Collections.sort(names1, (s1, s2) -> s1.compareTo(s2));
+	      Collections.sort(names1);
 	}
 
 }

7ff2d0f22dfad3fb8b3e645c4a1463aa99b3b6c9
hufei.shen <hufei.shen@ttpai.cn>
2019-12-23 16:52:32 +0800

    测试@JSONField注解的作用

---
+++ src/main/java/learn/json/DemoPojo2.java
@@ -0,0 +1,20 @@
+package learn.json;
+
+import java.util.Date;
+
+public class DemoPojo2 {
+	private String name;
+	private Date date;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}

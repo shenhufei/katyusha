141eaec4f15033ab8a6520f31bda073430f32cbe
hufei.shen <hufei.shen@ttpai.cn>
2020-01-21 15:42:17 +0800

    1

---
+++ src/main/java/learn/Demo2.java
@@ -0,0 +1,13 @@
+package learn;
+
+import learn.destroyobject.TestDemo1;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		TestDemo1 demo1 = new TestDemo1();
+		System.out.println(demo1.doSomething());
+		
+	}
+
+}

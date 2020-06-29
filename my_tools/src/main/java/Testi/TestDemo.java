53d0d28ff233df37f7093f17a07be3148a40f9ac
hufei.shen <hufei.shen@ttpai.cn>
2020-06-30 01:24:09 +0800

    修改子工程名称

---
+++ my_tools/src/main/java/Testi/TestDemo.java
@@ -0,0 +1,14 @@
+package Testi;
+
+public class TestDemo {
+
+    public static void main(String[] args) {
+        int i  = 1;
+        int j  = 1;
+        i = ++i;
+        j = j++;
+        System.out.println("i:"+i);
+        System.out.println("j:"+j);
+
+    }
+}

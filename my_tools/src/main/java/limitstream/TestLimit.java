2441e519f849c04fc9cad29b95126e06cbc7bd45
hufei.shen <hufei.shen@ttpai.cn>
2020-07-03 09:32:36 +0800

    调整版本控制

---
+++ my_tools/src/main/java/limitstream/TestLimit.java
@@ -0,0 +1,16 @@
+package limitstream;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+/**
+ * @author shenhufei
+ * @version 1.0
+ * @Description:
+ * @date 20200701
+ */
+public class TestLimit {
+    public static void main(String[] args) {
+        RateLimiter rateLimiter = RateLimiter.create(1);
+
+    }
+}
---
+++ my_tools/src/main/java/shanyan/TestShanYan.java
@@ -0,0 +1,10 @@
+package shanyan;
+
+/**
+ * @author shenhufei
+ * @version 1.0
+ * @Description:
+ * @date 20200630
+ */
+public class TestShanYan {
+}

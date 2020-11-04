2023063c3891491b9ad623a22e8a2b5f9e442ff9
hufei.shen <hufei.shen@ttpai.cn>
2020-11-04 10:04:00 +0800

    删除敏感信息

--- version_controller/src/main/java/com/shenhufei/Katyusha/core/InvokeMethodHandler.java
+++ version_controller/src/main/java/com/shenhufei/Katyusha/core/InvokeMethodHandler.java
@@ -37,7 +37,6 @@ public class InvokeMethodHandler implements InvokeHandler {
 	public Object doHandler(Request param) throws ClassNotFoundException, MethodNotFoundException, MethodCodeNotFoundException {
 		//TODO  获取前置方法，获取后置方法；环绕方法，这个执行的方法执行的粒度是针对于类还是针对于方法；
 		//TODO  添加自定义注解，注解用在类上面，表示这个
-
 		// 需要扫描，用户是否是实现了Filter接口，看是否需要执行响应的方法；
 		// 获取服务版本号
 		String versionString = MethodUtils.getMethodVersion(VersionHandler.getVersionMap(), param.getVersion(), param.getCode());
